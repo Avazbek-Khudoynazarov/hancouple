41864578aef221021ffd387e8f0d88f01cf9451e
Avazbek-Khudoynazarov <121384262+Avazbek-Khudoynazarov@users.noreply.github.com>
2025-10-23 15:09:09 +0900

    feat: bug resolved

--- app/introduction/page.tsx
+++ app/introduction/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import Introduction from "@/components/introduction";
 
 export default function IntroductionPage() {
-  return <Introduction />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Introduction />
+    </Suspense>
+  );
 }
