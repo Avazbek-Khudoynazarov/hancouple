34250116b931743015ec6d5e95b4b41e64a03d94
Avazbek-Khudoynazarov <121384262+Avazbek-Khudoynazarov@users.noreply.github.com>
2026-01-09 18:44:12 +0900

    fix: Update Branch Office Google Maps embed URL with correct location

--- src/components/introduction/Location.tsx
+++ src/components/introduction/Location.tsx
@@ -66,7 +66,7 @@ export default function Location() {
 
         <div className={styles.officeSection}>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3221.5!2d128.4784!3d36.0154!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2z6rK97IOB67aB64-EIOy5oOqzoeq1sCDsp4DsspzrqbQg6riI7Iah66GcIDYw!5e0!3m2!1sko!2skr!4v1704790000000"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1615.5!2d128.463924!3d35.9473995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3565e974db068adf%3A0x4bc5b6d6aab3489a!2z7JiB7KeE7KCE66y464yA7ZWZ6rWQIOq4gOuhnOuyhOy6oO2NvOyKpCDsnbjsoJzri4jsmoBr!5e0!3m2!1sko!2skr!4v1704790000000"
             className={styles.mapContainer}
             style={{ border: 0 }}
             allowFullScreen
